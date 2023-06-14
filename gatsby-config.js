f8db3ce22edc1f126d31783c1a5160fdc3f92fb0
discussionforall@gmail.com <discussionforall@gmail.com>
2023-06-14 12:31:11 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
