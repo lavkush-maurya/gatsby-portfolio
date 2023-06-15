b0963c0543a3eeaa77e9669a13104e2f8883db4c
discussionforall@gmail.com <discussionforall@gmail.com>
2023-06-15 17:59:30 +0530

    single-project-page

--- src/templates/projects-details.js
+++ src/templates/projects-details.js
@@ -1,11 +1,34 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { graphql } from "gatsby";
+import * as styles from "../styles/project-details.module.css";
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark;
+  const { title, stack } = data.markdownRemark.frontmatter;
+  console.log(data);
   return (
     <Layout>
-      <div>ProjectDetails</div>
-      <div>title</div>
+      <div className={styles.details}>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
+        <div
+          className={styles.html}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+      }
+    }
+  }
+`;
